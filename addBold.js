98c0df68b13fea42273eab5384bfc6486ab993b4
Paul Ronga <paul.ronga@laposte.net>
2017-12-26 12:31:53 +0100

    array “lines” instead of string

--- addBold.js
+++ addBold.js
@@ -12,95 +12,98 @@ function eraseFalseBreaks(textArea){ // Elimine les faux sauts de ligne de Metho
 }
 
 function subtitles(textArea){ // Mise en gras avec espacement
+  // save current text for “undo” function
   localStorage.setItem('initialText', textArea.value);
-  eraseFalseBreaks(textArea);
-  var txtNew = "";
-  var arr = textArea.value.split("\n");
 
-  for (i=0;i<arr.length;i++){
-    if(arr[i] == ""){
-      txtNew += arr[i] + '\n'; 			// Paragraphe vide: on passe
+  eraseFalseBreaks(textArea);
+  let lines = textArea.value.split("\n");
+
+  for (i=0;i<lines.length;i++){
+    if(lines[i] == ""){
+      // empty paragraph: pass
     }else{
-      if( ( !arr[i].match(/\./g) ) && ( !arr[i].match("<b>") ) ){ // No point in the paragraph; and it has no bold yet
-        txtNew += "<b>" + arr[i] + "</b>\n";
-      }else{
-        txtNew += arr[i] + "\n";
+      if( ( !lines[i].match(/\./g) ) && ( !lines[i].match("<b>") ) ){ // No point in the paragraph; and it has no bold yet
+        lines[i] = "<b>" + lines[i] + "</b>";
       }
+      // else: pass
     }
   }
-  textArea.value = txtNew;
+  textArea.value = lines.join('\n');
 }
 
 function interview(textArea){ // Mise en gras avec espacement
+  // save current text for “undo” function
   localStorage.setItem('initialText', textArea.value);
-  eraseFalseBreaks(textArea);
-  var txtNew = "";
-  var textAreaLines = textArea.value.split('\n');
-  var numberOfLines = textAreaLines.length;
-  var qrIndex = 0;
 
-  for (i=0;i<numberOfLines;i++)
+  eraseFalseBreaks(textArea);
+  let lines = textArea.value.split('\n');
+  let qaIndex = 0; // question-answer index, incremented for each question or answer
+
+  for (i=0;i<lines.length;i++)
   {
-    if(textAreaLines[i] == '')	// paragraphe vide: on passe
+    if(lines[i] == '')
     {
-      txtNew += textAreaLines[i] + '\n';
+      // pass
     }
     else
     {
-      if(qrIndex<1)	// avant la 1ere question
+      if(qaIndex<1)	// avant la 1ere question
       {
-        if((textAreaLines[i].match(/^.{1,240}\?$/g))&&(textAreaLines[i].substring(0,1)!='<'))	// Identification de la 1e question: chaîne de max. 240 caractères terminée par un point && ne commence pas par un tag html (= question déjà traitée, iframe)
+        if((lines[i].match(/^.{1,240}\?$/g))&&(lines[i].substring(0,1)!='<'))	// Identification de la 1e question: chaîne de max. 240 caractères terminée par un point && ne commence pas par un tag html (= question déjà traitée, iframe)
         {
-          txtNew += '\n<b>' + textAreaLines[i] + '</b><br />';
-          qrIndex++;
+          // add a line break
+          lines[i] = '\n<b>' + lines[i] + '</b><br />';
+          qaIndex++;
         }
-        else	// paragraphes avant l’interview: on traite et on passe
-        {
-          txtNew += textAreaLines[i] + '\n';
-        }
+        // else: paragraph before the iterview began: pass
 
       }
-      else	// qrIndex > 0: on est dans l’enchaînement question-réponse
+      else	// qaIndex > 0: we’re in the interview
       {
-        if((qrIndex%2==0)&&(textAreaLines[i].substring(0,3)!='<b>'))	// pair = question
+        if((qaIndex%2==0)&&(lines[i].substring(0,3)!='<b>'))	// even => question
         {
-          txtNew += '\n<b>' + textAreaLines[i] + '</b><br />'; //	gras + se termine par un saut de ligne
-          qrIndex++;
+          lines[i] = '\n<b>' + lines[i] + '</b><br />'; //	bold then newline
+          qaIndex++;
         }
-        else	// réponse: mise en forme normale
+        else	// answer: no bold
         {
-          qrIndex++;
-          txtNew += textAreaLines[i] + '\n';
+          qaIndex++;
         }
       }
     }
   }
-  textArea.value = txtNew;
+  textArea.value = lines.join('\n');
 }
 
 function factsheet(textArea){
+  console.log('fs')
+  // save current text for “undo” function
   localStorage.setItem('initialText', textArea.value);
-  var txtNew = "";
-  var arr = textArea.value.split("\n");
 
-  for (i=0;i<arr.length;i++){
-    // Paragraphe sans “:” ou contenant déjà un <b>: on passe
-    if( (arr[i].indexOf(':') < 0) && (arr[i].indexOf('<b>') < 0) ){
-      txtNew += arr[i] + '\n';
+  let lines = textArea.value.split("\n");
+
+  for ( i=0; i<lines.length; i++ ){
+    // Paragraph without “:”: we pass
+    if( lines[i].indexOf(':') < 0 ){
+      // pass
     }else{
-      var chain = arr[i].split(':'); // ^\d{4}$
+      let chain = lines[i].split(':'); // ^\d{4}$
       if(chain[0] == ''){
         chain.splice(0, 1);
       }
-      txtNew += '<b>'+chain[0]+'</b>';
+      if(chain[0].indexOf('<b>') >= 0){ // already contains <b>: don’t add bold again
+        lines[i] = chain[0];
+      }else{
+        lines[i] = '<b>'+chain[0]+'</b>';
+      }
+
       chainLength = chain.length;
       for(c=1;c<chainLength;c++){
-        txtNew += ':' + chain[c];
+        lines[i] += ':' + chain[c];
       }
-      txtNew += '\n';
     }
   }
-  textArea.value = txtNew;
+  textArea.value = lines.join('\n');
 }
 
 
@@ -119,11 +122,11 @@ if(activeElement){
   }
 }
 if(!elementFound){
-  var activeElement = document.getElementById('bodytext');
+  let activeElement = document.getElementById('bodytext');
   if(activeElement){
     console.log('Textarea found using id “bodytext”.')
   }else{
-    var taList = document.getElementsByTagName("TEXTAREA");
+    let taList = document.getElementsByTagName("TEXTAREA");
     if (taList.length > 0){
       activeElement = taList[0];
       console.log('First textarea selected.')
@@ -169,6 +172,10 @@ var macos = navigator.appVersion.indexOf("Mac") != -1;
 if (typeof shortcutsOn == 'undefined') {
   var shortcutsOn = true;
 }
+
+// TODO
+// replace with browser.commands
+/*
 if(shortcutsOn){
   console.log('Listening to shortcuts…')
   document.onkeydown = function(e){
@@ -198,12 +205,16 @@ if(shortcutsOn){
   }
 }else{
   document.onkeydown = null;
-}
+}*/
 
-/*
-Call the appropriate function.
-We could avoid it if we hadn’t to inject code using executeScript.
+/**
+* Call the appropriate function.
+* Sort of argument, the extension way
 */
+if (typeof method == 'undefined') {
+  var method = 'not set';
+  console.log('WARNING: Method was not set.')
+}
 
 if(activeElement.value != ''){
   switch(method) {
