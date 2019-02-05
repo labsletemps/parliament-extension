6acec63de218aac7a36102f689d8c9f3cae7099c
Paul Ronga <paul.ronga@laposte.net>
2019-02-05 18:23:50 +0100

    fix

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,48 @@
 /**
- Globals
+Globals
 */
 
-
-
 function lookupNames(){
-  p_list = document.querySelectorAll('p');
-  for(i in p_list){
-    if(p_list.innerHTML){
-      p_list[i].innerHTML = p_list[i].innerHTML.replace('Pierre Maudet', '<b class="modal-available">Pierre Maudet</b>');      
+  var articleTags = document.getElementsByTagName('article');
+  if (articleTags.length > 0){
+
+    // Le Temps
+    var p_list = articleTags[0].querySelectorAll('.article-content p');
+    var count0 = 0;
+    var count = 0;
+
+    for(i in p_list){
+      if(p_list[i].innerHTML){
+        count0++;
+        var innerHTML = p_list[i].innerHTML;
+        if(innerHTML.indexOf('Pierre Maudet') >= 0){
+          p_list[i].innerHTML = innerHTML.replace('Pierre Maudet', '<span class="modal-available" data-who="pierre-maudet">Pierre Maudet</span>');
+          count++;
+        }
+      }
     }
   }
+
+  setTimeout(function(){
+    ['mouseover', 'click'].forEach(function(e) {
+      var spanElements = document.querySelectorAll('span.modal-available');
+      if(spanElements.length > 0){
+        for(i in spanElements){
+          spanElements[i].addEventListener(e, eventHandler);
+        }
+      }
+    });
+  }, 200);
+
 }
+
 lookupNames();
+
+function eventHandler(e){
+  alert('Event!');
+}
+
+
 
 function log(obj){
   // toggle for debug
@@ -22,13 +52,13 @@ function log(obj){
 // To prevent an error when the extension loads at first.
 // We use “var” instead of “let” so it may be overwritten.
 chrome.storage.local.get({
- noShortcut: false
+  noShortcut: false
 }, function(items){
   noShortcut = items.noShortcut;
 
- if(noShortcut){
-   shortcutsOn = false;
- }
+  if(noShortcut){
+    shortcutsOn = false;
+  }
 });
 
 if (typeof shortcutsOn == 'undefined') {
