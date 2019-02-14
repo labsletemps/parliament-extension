ef78d56e8a715a85d268c3bc13461e0f05f5175a
Paul Ronga <paul.ronga@laposte.net>
2019-02-14 17:43:40 +0100

    fetch & store lobbywatch individual data

--- contentScript.js
+++ contentScript.js
@@ -6,8 +6,22 @@ var parliamentData;
 var nameList = [];
 var localData = localStorage.getItem('parliamentarians');
 
-function getParliamentarianAttr(name){
+var currentPeople = {};
+
+function getDataWho(name){
+  name.normalize('NFD').replace(/[\u0300-\u036f]/g, "")   // on enleve les accents
   return name.replace(' ', '-').toLowerCase();
+}
+
+function getParti(data){
+  if('partyMembership' in data){
+    if ('party' in data['partyMembership']){
+      if ('abbr' in data['partyMembership']['party']){
+        return data['partyMembership']['abbr']
+      }
+    }
+  }
+  return '';
 }
 
 function getIndividualData(name){
@@ -25,7 +39,7 @@ function fetchParliamentIds(){
   $.ajax({
     method: "POST",
     url: "https://web.tcch.ch/parliament/",
-    success : function(data, statut){ // code_html contient le HTML renvoyé
+    success : function(data, statut){
       localStorage.setItem('parliamentarians', data);
       data['data']['parliamentarians'].forEach(function(item){
         nameList.push(item['name']);
@@ -48,14 +62,11 @@ if(localData){
   console.log('loaded from server')
 }
 
-
 function lookupNames(){
   var articleTags = $('.article-content p');
 
   if (articleTags.length > 0){
-
-    // Le Temps
-    var p_list = $('.article-content p');
+    var p_list = $('.article-content p');     // Le Temps
     var count0 = 0;
     var count = 0;
 
@@ -66,22 +77,14 @@ function lookupNames(){
 
         nameList.forEach(function(name){
           if(innerHTML.indexOf(name) !== -1){
-            console.log('FOUND::' + name + '!!!!!');
-            p_list[i].innerHTML = innerHTML.replace(name, '<span class="modal-available" data-who="pierre-maudet">' + name + '</span>');
-
+            var dataWho = getDataWho(name);
+            currentPeople[dataWho] = name;
+            p_list[i].innerHTML = innerHTML.replace(name, '<span class="modal-available ' + dataWho + '" data-who="' + dataWho + '">' + name + '</span>');
           }
         });
-
-        if(innerHTML.indexOf('Pierre Maudet') >= 0){
-          var name = 'Pierre Maudet';
-          var nameReplace = 'Pierre Maudet';
-          p_list[i].innerHTML = innerHTML.replace(name, '<span class="modal-available" data-who="pierre-maudet">' + nameReplace + '</span>');
-          count++;
-        }
       }
     }
   }
-
 
   setTimeout(function(){
     $('span.modal-available').on('click mouseover', function(e){
@@ -89,30 +92,39 @@ function lookupNames(){
       eventHandler($(this).data('who'));
     });
   }, 200);
-
 }
 
 lookupNames();
 
-var data = {
-  'pierre-maudet': {
-    'name': 'Pierre Maudet',
-    'parti': 'PLR'
-  }
-}
-
 function eventHandler(who){
-  console.log(who);
-  if (Object.keys(data).indexOf(who) < 0){
+  var individualData = getIndividualData(currentPeople[who]);
+  if(! individualData){
     console.log('Not found in data')
     return false;
+  }else{
+    console.log('Data found for ' + currentPeople[who]);
   }
-  tippy('.modal-available', {
-    content: '<strong>' + data[who]['name'] + '</strong><p>' + data[who]['parti'] + '</p>',
+  var parti = getParti(individualData);
 
+  function addInfo(item){
+    return '<p class="individual-info">' + item + '</p>';
+  }
+  function addListItem(item){
+    return '<p class="individual-list-item">' + item + '</p>';
+  }
+  var content = '<h3>' + individualData['name'] + '</h3>';
+  content += '<img class="individual-img" src="' + individualData['portrait'] + '" alt="' + individualData['name'] + '">';
+  content += addInfo(individualData['councilTitle']);
+  content += addInfo(individualData['canton']);
+  if(parti) {
+    content += addInfo(parti);
+  }
+
+  tippy('.modal-available.' + who, {
+    content: content,
     aria: null,
     autoFocus: false,
-    trigger: 'mouseenter click',
+    trigger: 'click', // mouseenter
     appendTo: ref => ref.parentNode,
     onMount({ reference }) {
       reference.setAttribute('aria-expanded', 'true')
@@ -126,13 +138,106 @@ function eventHandler(who){
     distance: 7,
     animation: 'fade',
     theme: 'light-border dropdown',
-    updateDuration: 0
+    updateDuration: 0,
+
+    onShow(instance) {
+      // TODO check if local version up to date
+      var localIndividualData = localStorage.getItem(who);
+
+      if(localIndividualData){
+        data = JSON.parse(localIndividualData);
+        content += addInfo('<b>Liens d’intérêt</b>')
+        data['data']['getParliamentarian']['connections'].forEach(function(item){
+          content += addInfo(item['to']['name'])
+        });
+        instance.setContent(content);
+
+      }else{
+        var load = {"query":
+          query,
+          "variables":{"locale":"fr","id": individualData['id']}
+        }
+
+        $.ajax({
+          method: "POST",
+          url: "https://lobbywatch.ch/graphql",
+          contentType: "application/json",
+          data: JSON.stringify(load),
+          success : function(data, statut){ // code_html contient le HTML renvoyé
+            console.log('done')
+            console.log(data);
+            localStorage.setItem(who, JSON.stringify(data));
+            content += addInfo('<b>Liens d’intérêt</b>')
+            data['data']['getParliamentarian']['connections'].forEach(function(item){
+              content += addInfo(item['to']['name'])
+            })
+            instance.setContent(content);
+          },
+          error : function() {
+            console.log('error')
+          }
+        });
+
+      }
+
+
+    }
   });
 }
 
-
-
-function log(obj){
-  // toggle for debug
-  // console.log(obj);
-}
+var query = `query getParliamentarian($locale: Locale!, $id: ID!) {
+  getParliamentarian(locale: $locale, id: $id) {
+    name
+    active
+    canton
+    represents
+    parliamentId
+    dateOfBirth
+    gender
+    represents
+    councilJoinDate
+    councilTenure
+    age
+    occupation
+    commissions {
+      name
+    }
+    guests {
+      id
+      name
+    }
+    connections {
+      group
+      potency
+      function
+      compensation {
+        money
+        description
+        __typename
+      }
+      from {
+        __typename
+      }
+      to {
+        __typename
+        ... on Organisation {
+          id
+          name
+          __typename
+        }
+      }
+      vias {
+        __typename
+        to {
+          ... on Guest {
+            id
+            name
+            __typename
+          }
+          __typename
+        }
+      }
+      __typename
+    }
+  }
+}`;
